b98c2d3b31471b20dba2eb8b8fe33853428e4794
Damian Smutek <damian.smutek@makingwaves.com>
2019-01-07 11:16:52 +0100

    Iniitial

---
+++ src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="footer">
+    <h1>footer Component</h1>
+  </section>
+</template>
+
+<script lang="js">
+export default {
+  name: 'Footer'
+}
+</script>
+
+<style scoped>
+  .footer {
+
+  }
+</style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,34 @@
+<template lang="html">
+
+  <section class="navigation">
+    <h1>navigation Component</h1>
+  </section>
+
+</template>
+
+<script lang="js">
+  export default  {
+    name: 'navigation',
+    props: [],
+    mounted() {
+
+    },
+    data() {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+}
+</script>
+
+<style scoped lang="scss">
+  .navigation {
+
+  }
+</style>
